cc358c8e7731b4ea7aa38889472b8a38b3839dcb
andyrolfes <andyrolfes@gmail.com>
2015-11-20 22:47:32 -0500

    Updates, Monster Bash
    Updating Monster Mash and newest scene, Monster Bash, which replaces the
    floor with the hand. Monster Mash is currently the latest version of the
    build.
    
    Current R&D: "collecting" clouds, score-keeping.

---
+++ Assets/ADDITIONAL/Scripts/lookAT.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookAT : MonoBehaviour {
+
+	public Transform target;
+	
+	void Update() {
+		transform.LookAt (target);
+	}
+}
---
+++ Assets/ADDITIONAL/Scripts/lookAT.js
@@ -0,0 +1,7 @@
+﻿// Drag another object onto it to make the camera look at it.
+var target : Transform; 
+
+// Rotate the camera every frame so it keeps looking at the target 
+function Update() {
+    transform.LookAt(target);
+}
